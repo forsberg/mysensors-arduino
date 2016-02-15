7319e75c833cc9aa4ba572afab02673c04c5e095
Erik Forsberg <forsberg@efod.se>
2016-02-15 17:53:12 +0100

    Report battery voltage. Use Adafruit DHT library.

--- Vinden/Vinden.ino
+++ Vinden/Vinden.ino
@@ -8,7 +8,9 @@
 #define CHILD_ID_TEMP0 1
 #define CHILD_ID_HUM1 2
 #define CHILD_ID_TEMP1 3
+#define CHILD_ID_BATTERYVOLTAGE 4
 
+#define DHTTYPE DHT22
 #define HUMIDITY_SENSOR0_DIGITAL_PIN 5 // Inside the box
 #define HUMIDITY_SENSOR1_DIGITAL_PIN 6 // Outside the box
 unsigned long SLEEP_TIME = 30000; // Sleep time between reads (in milliseconds)
@@ -18,8 +20,8 @@ MyHwATMega328 hw; // Select AtMega328 hardware profile
 MySigningAtsha204Soft signer;
 
 MySensor gw(radio, hw, signer); 
-DHT dht0;
-DHT dht1;
+DHT dht0(HUMIDITY_SENSOR0_DIGITAL_PIN, DHTTYPE);
+DHT dht1(HUMIDITY_SENSOR1_DIGITAL_PIN, DHTTYPE);
 float lastTemp0;
 float lastHum0;
 float lastTemp1;
@@ -29,6 +31,7 @@ boolean metric = true;
 boolean sentValue = false;
 MyMessage msgHum(CHILD_ID_HUM0, V_HUM);
 MyMessage msgTemp(CHILD_ID_TEMP0, V_TEMP);
+MyMessage msgBatteryVoltage(CHILD_ID_BATTERYVOLTAGE, V_VOLTAGE);
 
 int oldBatteryPcnt = 0;
 
@@ -45,6 +48,7 @@ void setup()
 
   // Register all sensors to gw (they will be created as child devices)
 
+  gw.present(CHILD_ID_BATTERYVOLTAGE, S_MULTIMETER);
   gw.present(CHILD_ID_HUM0, S_HUM);
   gw.present(CHILD_ID_TEMP0, S_TEMP);
   gw.present(CHILD_ID_HUM1, S_HUM);
@@ -55,10 +59,10 @@ void setup()
 
 void loop()      
 {  
-  delay(dht0.getMinimumSamplingPeriod());
+  delay(2000);
 
   // Read Temperature from sensor 0
-  float temperature = dht0.getTemperature();
+  float temperature = dht0.readTemperature();
   if (isnan(temperature)) {
       Serial.println("Failed reading temperature from DHT0");
   } else if (temperature != lastTemp0) {
@@ -70,7 +74,7 @@ void loop()
   }
 
   // Read humidity from sensor 0
-  float humidity = dht0.getHumidity();
+  float humidity = dht0.readHumidity();
   if (isnan(humidity)) {
       Serial.println("Failed reading humidity from DHT0");
   } else if (humidity != lastHum0) {
@@ -82,7 +86,7 @@ void loop()
   }
 
   // Read Temperature from sensor 1
-  temperature = dht1.getTemperature();
+  temperature = dht1.readTemperature();
   if (isnan(temperature)) {
       Serial.println("Failed reading temperature from DHT1");
   } else if (temperature != lastTemp1) {
@@ -94,7 +98,7 @@ void loop()
   }
 
   // Read humidity from sensor 1
-  humidity = dht1.getHumidity();
+  humidity = dht1.readHumidity();
   if (isnan(humidity)) {
       Serial.println("Failed reading humidity from DHT1");
   } else if (humidity != lastHum1) {
@@ -114,8 +118,8 @@ void loop()
    // 10.32/1023 = Volts per bit = 0.010089224433768015
    float batteryV  = batterySensorValue * 0.010089224433768015;
    
-   float batteryVremaining = batteryV - 3.7;
-   int batteryPcnt = 100 * batteryVremaining / (4.2-3.7);   
+   float batteryVremaining = batteryV - 3.3;
+   int batteryPcnt = 100 * batteryVremaining / (4.2-3.3);   
 
    Serial.print("Battery Voltage: ");
    Serial.print(batteryV);
@@ -130,6 +134,7 @@ void loop()
      // Power up radio after sleep
      gw.sendBatteryLevel(batteryPcnt);
      oldBatteryPcnt = batteryPcnt;
+     gw.send(msgBatteryVoltage.set(batteryV, 2));
    }  
 
   gw.sleep(SLEEP_TIME); //sleep a bit
